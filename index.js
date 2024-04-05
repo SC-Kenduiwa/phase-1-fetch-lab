5807a8a17c32fa7cb43928923677bf62b18dce16
Shanice Chebet Kenduiwa <kenduiwashanice63@gmail.com>
2024-04-05 09:48:44 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  // Make a fetch request to the Game of Thrones API
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())//convert the response JSON
+  .then(json => renderBooks(json));
+  } 
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
